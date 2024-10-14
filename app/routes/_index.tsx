1e9cbbbe4e0d0fd8cfad9bc89cef9beb1a4bd86d
Reid Cole <reid-cole@outlook.com>
2024-10-14 14:36:04 -0400

    added script tag that shouldn't work

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -3,14 +3,23 @@ import { useState } from "react";
 export default function Index() {
 	const [count, setCount] = useState(0);
 	return (
-		<div>
-			<div>count: {count}</div>
-			<button
-				className="border rounded-md px-2"
-				onClick={() => setCount((prev) => prev + 1)}
-			>
-				increment
-			</button>
-		</div>
+		<>
+			<div>
+				<div>count: {count}</div>
+				<button
+					className="border rounded-md px-2"
+					onClick={() => setCount((prev) => prev + 1)}
+				>
+					increment
+				</button>
+			</div>
+
+			<script
+				dangerouslySetInnerHTML={{
+					__html:
+						'alert("This script should not run because it has no nonce set.")',
+				}}
+			/>
+		</>
 	);
 }
